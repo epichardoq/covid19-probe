32facdaf7fab9d16a918c03169141b479d40b8dd
epichardoq <epq9406@gmail.com>
2020-12-12 23:48:00 -0600

    Create a scripts folder and a python file

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,9 @@
+Python 3.7.4 (default, Sep  7 2019, 20:23:23) 
+[Clang 9.1.0 (clang-902.0.39.2)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello word!")
+Hello word!
+>>> 
+print ("second change")
+second change
+>>> 
